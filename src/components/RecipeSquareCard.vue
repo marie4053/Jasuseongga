76046eb886e6da0280c394516a2f9f7e0c4a6ac4
marie4053 <marie061313@gmail.com>
2025-02-28 09:40:07 +0900

    Chore: tailwind prefix 제거

--- src/components/RecipeSquareCard.vue
+++ src/components/RecipeSquareCard.vue
@@ -12,34 +12,25 @@
 
 <template>
   <div
-    :class="
-      large
-        ? 'tw:h-[460px] tw:w-[460px]'
-        : medium
-          ? 'tw:h-[380px] tw:w-[380px]'
-          : 'tw:h-[300px] tw:w-[300px]'
-    "
-    class="tw:overflow-hidden tw:rounded-[20px]"
+    :class="large ? 'h-[460px] w-[460px]' : medium ? 'h-[380px] w-[380px]' : 'h-[300px] w-[300px]'"
+    class="overflow-hidden rounded-[20px]"
   >
-    <div
-      :style="{backgroundImage: `url(${props.image})`}"
-      class="tw:w-full tw:h-full tw:bg-cover tw:bg-center"
-    >
+    <div :style="{backgroundImage: `url(${props.image})`}" class="w-full h-full bg-cover bg-center">
       <div
         :class="
           large
-            ? 'tw:bg-linear-to-t tw:from-mono-900/50 tw:to-mono-900/0 tw:to-[50%] tw:!px-[40px] tw:!py-[32px]'
-            : 'tw:bg-linear-to-t tw:from-mono-900/40 tw:to-mono-900/0 tw:to-[42%] tw:!px-[24px] tw:!py-[16px]'
+            ? 'bg-linear-to-t from-mono-900/50 to-mono-900/0 to-[50%] px-[40px] py-[32px]'
+            : 'bg-linear-to-t from-mono-900/40 to-mono-900/0 to-[42%] px-[24px] py-[16px]'
         "
-        class="tw:w-full tw:h-full tw:text-mono-050 tw:flex tw:items-end"
+        class="w-full h-full text-mono-050 flex items-end"
       >
-        <div class="tw:flex tw:flex-col tw:gap-1">
-          <div v-if="props.subtitle" class="tw:text-[20px] tw:leading-[24px] tw:font-medium">
+        <div class="flex flex-col gap-1">
+          <div v-if="props.subtitle" class="text-[20px] leading-[24px] font-medium">
             {{ props.subtitle }}
           </div>
           <div
-            :class="large ? 'tw:text-[32px] tw:leading-[38px]' : 'tw:text-[36px] tw:leading-[42px]'"
-            class="tw:font-bold"
+            :class="large ? 'text-[32px] leading-[38px]' : 'text-[36px] leading-[42px]'"
+            class="font-bold"
           >
             {{ props.title }}
           </div>
--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -59,27 +59,27 @@
 
 <template>
   <!-- 배너 -->
-  <div class="tw:w-full tw:h-[562px] tw:overflow-hidden">
+  <div class="w-full h-[562px] overflow-hidden">
     <div
-      class="tw:bg-[url(https://images.unsplash.com/photo-1497888329096-51c27beff665?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] tw:w-full tw:h-full tw:bg-cover tw:bg-center"
+      class="bg-[url(https://images.unsplash.com/photo-1497888329096-51c27beff665?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] w-full h-full bg-cover bg-center"
     >
-      <div class="tw:w-full tw:h-full tw:bg-mono-900/30 tw:flex tw:place-content-center">
-        <div class="tw:flex tw:flex-col tw:gap-10 tw:justify-center">
+      <div class="w-full h-full bg-mono-900/30 flex place-content-center">
+        <div class="flex flex-col gap-10 justify-center">
           <!-- 타이틀 -->
           <div>
-            <div class="tw:text-[40px] tw:text-mono-050 tw:leading-10">혼자 살아도 건강하게!</div>
-            <div class="tw:text-[48px] tw:font-bold tw:text-mono-050 tw:leading-16">
+            <div class="text-[40px] text-mono-050 leading-10">혼자 살아도 건강하게!</div>
+            <div class="text-[48px] font-bold text-mono-050 leading-16">
               자취생을 위한 영양만점 레시피
             </div>
           </div>
           <!-- 검색바 -->
           <div
-            class="tw:bg-mono-050 tw:w-[1060px] tw:h-[64px] tw:rounded-[100px] tw:flex tw:items-center tw:justify-between tw:!px-[30px]"
+            class="bg-mono-050 w-[1060px] h-[64px] rounded-[100px] flex items-center justify-between px-[30px]"
           >
             <input
               type="text"
               placeholder="검색어를 입력하세요"
-              class="tw:h-[30px] tw:w-[900px] text tw:placeholder:text-[20px] tw:outline-none tw:!text-[20px]"
+              class="h-[30px] w-[900px] text placeholder:text-[20px] outline-none text-[20px]"
             />
             <button @click="handleSearch">
               <v-icon color="grey-darken-1" size="36px">mdi-magnify</v-icon>
@@ -89,45 +89,45 @@
       </div>
     </div>
   </div>
-  <div class="container tw:flex tw:flex-col tw:gap-[100px] tw:!pt-[100px]">
+  <div class="container flex flex-col gap-[100px] pt-[100px]">
     <!-- 카테고리별 레시피 -->
-    <div class="tw:flex tw:flex-col tw:gap-[28px]">
-      <div class="ft-point tw:text-[48px] tw:text-mono-700">카테고리별 레시피</div>
-      <div class="tw:flex tw:justify-between">
+    <div class="flex flex-col gap-[28px]">
+      <div class="ft-point text-[48px] text-mono-700">카테고리별 레시피</div>
+      <div class="flex justify-between">
         <template v-for="item in recipeCategoryData" :key="item.category">
           <RecipeSquareCard :title="item.category" :image="item.image" />
         </template>
       </div>
     </div>
     <!-- 오늘의 레시피 -->
-    <div class="tw:flex tw:flex-col tw:gap-[28px]">
-      <div class="ft-point tw:text-[48px] tw:text-mono-700">오늘의 레시피</div>
-      <div class="tw:flex tw:justify-between">
+    <div class="flex flex-col gap-[28px]">
+      <div class="ft-point text-[48px] text-mono-700">오늘의 레시피</div>
+      <div class="flex justify-between">
         <RecipeSquareCard
           :title="todaysRecipe.title"
           :subtitle="todaysRecipe.subtitle"
           :image="todaysRecipe.image"
           large
         />
-        <div class="tw:flex tw:flex-col tw:w-[1040px] tw:justify-between tw:text-mono-700">
-          <div class="tw:flex tw:flex-col tw:gap-4">
-            <div class="tw:text-[32px] tw:font-bold">레시피 설명</div>
-            <p class="tw:text-[20px] tw:leading-[24px]">{{ todaysRecipe.description }}</p>
+        <div class="flex flex-col w-[1040px] justify-between text-mono-700">
+          <div class="flex flex-col gap-4">
+            <div class="text-[32px] font-bold">레시피 설명</div>
+            <p class="text-[20px] leading-[24px]">{{ todaysRecipe.description }}</p>
           </div>
-          <div class="tw:flex tw:flex-col tw:gap-2">
-            <div class="tw:text-[32px] tw:font-bold">영양정보</div>
-            <p class="tw:text-[20px] tw:leading-[24px] tw:text-mono-400">
+          <div class="flex flex-col gap-2">
+            <div class="text-[32px] font-bold">영양정보</div>
+            <p class="text-[20px] leading-[24px] text-mono-400">
               1일 영양성분 기준치에 대한 비율 (%)
             </p>
-            <div class="tw:bg-main-50 tw:h-[200px]"></div>
+            <div class="bg-main-50 h-[200px]"></div>
           </div>
         </div>
       </div>
     </div>
     <!-- 냉장고 속 재료별 레시피 -->
-    <div class="tw:flex tw:flex-col tw:gap-[28px]">
-      <div class="ft-point tw:text-[48px] tw:text-mono-700">냉장고 속 재료별 레시피</div>
-      <div class="tw:flex tw:justify-between">
+    <div class="flex flex-col gap-[28px]">
+      <div class="ft-point text-[48px] text-mono-700">냉장고 속 재료별 레시피</div>
+      <div class="flex justify-between">
         <template v-for="item in recipeIngredientData" :key="item.category">
           <RecipeSquareCard :title="item.category" :image="item.image" medium />
         </template>
