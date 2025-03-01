d6ef2637b4c0d2c6157bdd8a099d1915980b5a8a
marie4053 <marie061313@gmail.com>
2025-03-02 02:14:25 +0900

    Design: RecipePostList 컴포넌트 수정

--- src/components/recipe/RecipePostList.vue
+++ src/components/recipe/RecipePostList.vue
@@ -10,16 +10,18 @@
 </script>
 
 <template>
-  <div class="flex h-[124px] w-[788px] justify-between">
-    <div class="w-[124px] rounded-[16px] overflow-hidden">
+  <div class="flex h-[124px] w-full">
+    <div class="min-w-[124px] w-[124px] rounded-[16px] overflow-hidden shrink-0">
       <div
         :style="{backgroundImage: `url(${props.image})`}"
         class="w-full h-full bg-cover bg-center"
       ></div>
     </div>
-    <div class="flex flex-col gap-[8px] w-[648px]">
-      <div class="text-[18px] text-mono-400">{{ props.author }}</div>
-      <div class="text-[32px] text-mono-700 font-semibold leading-[38px]">{{ props.title }}</div>
+    <div class="flex flex-col gap-[8px] ml-4 overflow-hidden flex-1">
+      <div class="text-[18px] text-mono-400 truncate">{{ props.author }}</div>
+      <div class="text-[32px] text-mono-700 font-semibold leading-[38px] line-clamp-1">
+        {{ props.title }}
+      </div>
       <div class="text-[18px] text-mono-700 line-clamp-1">{{ props.content }}</div>
     </div>
   </div>
