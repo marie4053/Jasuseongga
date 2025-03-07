98a7c81488b9d8970e85da1f0d2d1aeea3b90056
marie4053 <86790183+marie4053@users.noreply.github.com>
2025-03-07 11:25:10 +0900

    Feat: 레시피 이미지 최적화 및 폴더 구조 변경 (#66)
    Co-authored-by: marie4053 <marie061313@gmail.com>

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -9,44 +9,44 @@
   const recipeCategoryData = [
     {
       category: '밥',
-      image: '/recipe/recipe_category_rice.webp',
+      image: '/images/recipe/recipe_category_rice.webp',
     },
     {
       category: '반찬',
-      image: '/recipe/recipe_category_side.webp',
+      image: '/images/recipe/recipe_category_side.webp',
     },
     {
       category: '국',
-      image: '/recipe/recipe_category_soup.webp',
+      image: '/images/recipe/recipe_category_soup.webp',
     },
     {
       category: '일품',
-      image: '/recipe/recipe_category_one_dish.webp',
+      image: '/images/recipe/recipe_category_one_dish.webp',
     },
     {
       category: '후식',
-      image: '/recipe/recipe_category_dessert.webp',
+      image: '/images/recipe/recipe_category_dessert.webp',
     },
   ];
   const recipeIngredientData = [
     {
       category: '육류',
-      image: '/recipe/recipe_ingredient_meat.webp',
+      image: '/images/recipe/recipe_ingredient_meat.webp',
       ingredient: '고기',
     },
     {
       category: '채소',
-      image: '/recipe/recipe_ingredient_vegetable.webp',
+      image: '/images/recipe/recipe_ingredient_vegetable.webp',
       ingredient: '양파',
     },
     {
       category: '해산물',
-      image: '/recipe/recipe_ingredient_seafood.webp',
+      image: '/images/recipe/recipe_ingredient_seafood.webp',
       ingredient: '새우',
     },
     {
       category: '과일',
-      image: '/recipe/recipe_ingredient_fruit.webp',
+      image: '/images/recipe/recipe_ingredient_fruit.webp',
       ingredient: '사과',
     },
   ];
@@ -55,7 +55,7 @@
     subtitle: '오늘의 추천 레시피',
     description:
       '소고기 들깨 알토란탕은 고소한 들깨가루와 부드러운 소고기, 그리고 특유의 식감이 일품인 알토란을 주재료로 한 영양가 높은 한식 탕입니다. 알토란의 아삭한 식감과 들깨의 고소함이 어우러져 깊은 맛을 내며, 소고기의 감칠맛이 국물에 배어 풍미를 더합니다.',
-    image: '/recipe/recipe_todays.webp',
+    image: '/images/recipe/recipe_todays.webp',
     nutrition: {
       calories: 146.42,
       sodium: 675.68,
@@ -65,9 +65,9 @@
     },
   };
   const popularRecipeData = [
-    {title: '코코넛워터 토마토카레', image: '/recipe/recipe_popular3.webp'},
-    {title: '저염된장 삼치구이', image: '/recipe/recipe_popular1.webp'},
-    {title: '참나물 소보로덮밥', image: '/recipe/recipe_popular2.webp'},
+    {title: '코코넛워터 토마토카레', image: '/images/recipe/recipe_popular3.webp'},
+    {title: '저염된장 삼치구이', image: '/images/recipe/recipe_popular1.webp'},
+    {title: '참나물 소보로덮밥', image: '/images/recipe/recipe_popular2.webp'},
   ];
   const recipeCommunityData = [
     {
@@ -75,25 +75,25 @@
       title: '5분 완성! 참치마요 덮밥',
       content:
         '따뜻한 밥 위에 기름 뺀 참치와 마요네즈를 섞어 올리면 완성! 한 끼 식사가 부담스러울 때 5분이면 뚝딱 만들 수 있어요. 기호에 따라 김가루나 쪽파를 뿌리면 풍미가 더해지고, 매콤한 맛을 원한다면 고추장이나 sriracha 소스를 살짝 넣어도 좋아요.',
-      image: '/recipe/recipe_category_one_dish.webp',
+      image: '/images/recipe/recipe_category_one_dish.webp',
     },
     {
       author: '방구석셰프',
       title: '5분 완성! 참치마요 덮밥',
       content: '자취생 필수템, 참치캔과 마요네즈로 만드는 초간단 덮밥!',
-      image: '/recipe/recipe_category_one_dish.webp',
+      image: '/images/recipe/recipe_category_one_dish.webp',
     },
     {
       author: '방구석셰프',
       title: '5분 완성! 참치마요 덮밥',
       content: '자취생 필수템, 참치캔과 마요네즈로 만드는 초간단 덮밥!',
-      image: '/recipe/recipe_category_one_dish.webp',
+      image: '/images/recipe/recipe_category_one_dish.webp',
     },
     {
       author: '방구석셰프',
       title: '5분 완성! 참치마요 덮밥',
       content: '자취생 필수템, 참치캔과 마요네즈로 만드는 초간단 덮밥!',
-      image: '/recipe/recipe_category_one_dish.webp',
+      image: '/images/recipe/recipe_category_one_dish.webp',
     },
   ];
 
@@ -106,7 +106,7 @@
 <template>
   <!-- 배너 -->
   <div class="w-full h-[562px] overflow-hidden">
-    <div class="bg-[url(/recipe/recipe_main_banner.webp)] w-full h-full bg-cover bg-center">
+    <div class="bg-[url(/images/recipe/recipe_main_banner.webp)] w-full h-full bg-cover bg-center">
       <div class="w-full h-full bg-mono-900/30 flex place-content-center">
         <div class="flex flex-col gap-10 justify-center">
           <!-- 타이틀 -->
@@ -231,7 +231,7 @@
       class="container h-[230px] w-full bg-main-50 rounded-[12px] p-[72px] flex items-center justify-between cursor-pointer"
       @click="() => router.push('/community/recipe')"
     >
-      <div class="w-[200px] h-[160px] bg-[url(/recipe/recipe_illustration.svg)]"></div>
+      <div class="w-[200px] h-[160px] bg-[url(/images/recipe/recipe_illustration.svg)]"></div>
       <div class="flex flex-col items-end">
         <div class="text-main-500 text-[48px] font-bold">나만의 레시피를 함께 공유해보세요!</div>
         <div class="flex gap-[12px] items-center">
